6ab92431e5e3fd6436f16a27b8628796bdd9e9be
Adamhanafi22 <adamhanafi1922@gmail.com>
2025-01-25 10:11:47 +0700

    memperbaiki modelReservasi

--- app/src/main/java/com/example/projectakhir/model/Reservasi.kt
+++ app/src/main/java/com/example/projectakhir/model/Reservasi.kt
@@ -1,6 +1,32 @@
 package com.example.projectakhir.model
 
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+
+
 import kotlinx.serialization.*
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.text.SimpleDateFormat
+import java.util.Date
+
+// Custom Serializer for Date
+object DateSerializer : KSerializer<Date> {
+    private val dateFormat = SimpleDateFormat("yyyy-MM-dd") // Set your date format here
+
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("Date", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): Date {
+        val dateStr = decoder.decodeString()
+        return dateFormat.parse(dateStr) ?: throw SerializationException("Invalid date format")
+    }
+
+    override fun serialize(encoder: Encoder, value: Date) {
+        val dateStr = dateFormat.format(value)
+        encoder.encodeString(dateStr)
+    }
+}
 
 @Serializable
 data class Reservasi(
@@ -10,9 +36,11 @@ data class Reservasi(
 
     val id_pelanggan: Int,  // Foreign Key
 
-    val check_in: String,  // Tanggal check-in
+    @Serializable(with = DateSerializer::class)
+    val check_in: Date,  // Tanggal check-in
 
-    val check_out: String,  // Tanggal check-out
+    @Serializable(with = DateSerializer::class)
+    val check_out: Date,  // Tanggal check-out
 
     val jumlah_kamar: Int
 )
@@ -30,3 +58,4 @@ data class ReservasiDetailResponse(
     val message: String,
     val data: Reservasi
 )
+
